8861624f83d35baa12aa638f3f64e785ca1ab154
Lucas Oliveira10 <lucasoliveira13125@gmail.com>
2026-01-30 18:04:47 -0300

    feat(processor): Adicionando busca de arquivos csv/txt(encontrar_arquivo_csv)

--- src/processor.py
+++ src/processor.py
@@ -21,3 +21,15 @@ def normalizar_colunas(df):
 
     df.rename(columns=mapa_colunas, inplace=True)
     return df
+
+
+def encontrar_arquivo_csv(zip_ref):
+   
+    for nome_arquivo in zip_ref.namelist():
+
+        if nome_arquivo.endswith('/') or '__MACOSX' in nome_arquivo:
+            continue
+        
+        if nome_arquivo.lower().endswith('.csv') or nome_arquivo.lower().endswith('.txt'):
+            return nome_arquivo
+    return None
